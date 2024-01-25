2b186e2ea4b8bed65bc189de1d401448633b913b
5DONG <odj3460@gmail.com>
2024-01-25 20:56:42 +0900

    7d hardware

---
+++ kortex_bringup/launch/gen3_nogripper_7d.launch.py
@@ -0,0 +1,110 @@
+# Copyright (c) 2021 PickNik, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Denis Stogl
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_type",
+            default_value="gen3",
+            description="Type/series of robot.",
+            choices=["gen3", "gen3_lite"],
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_ip",
+            description="IP address by which the robot can be reached.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("dof", default_value="7", description="DoF of robot.")
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
+            Used only if 'use_fake_hardware' parameter is true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="joint_trajectory_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="ros2_controllers.yaml",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_internal_bus_gripper_comm",
+            default_value="true",
+            description="Use internal bus for gripper communication?",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+
+    # Initialize Arguments
+    robot_type = LaunchConfiguration("robot_type")
+    robot_ip = LaunchConfiguration("robot_ip")
+    dof = LaunchConfiguration("dof")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+    robot_controller = LaunchConfiguration("robot_controller")
+    use_internal_bus_gripper_comm = LaunchConfiguration("use_internal_bus_gripper_comm")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    controllers_file = LaunchConfiguration("controllers_file")
+
+    base_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ThisLaunchFileDir(), "/kortex_control_7d.launch.py"]),
+        launch_arguments={
+            "robot_type": robot_type,
+            "robot_ip": robot_ip,
+            "dof": dof,
+            "use_fake_hardware": use_fake_hardware,
+            "fake_sensor_commands": fake_sensor_commands,
+            "robot_controller": robot_controller,
+            "use_internal_bus_gripper_comm": use_internal_bus_gripper_comm,
+            "launch_rviz": launch_rviz,
+            "controllers_file": controllers_file,
+            "description_file": "gen3_7d.xacro",
+        }.items(),
+    )
+
+    return LaunchDescription(declared_arguments + [base_launch])
